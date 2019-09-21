f486d70cee267cf988715f520664ee46259b2a3c
yaoyu bao <yaobao5354@gmail.com>
2019-09-21 00:35:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,6 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="), value)}
-
-    end
     #@name = student_hash[:name]
     #@location = student_hash[:location]
     #@profile_url = student_hash[:profile_url]
