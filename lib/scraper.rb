59da6db14bf00dda751de9374e49f2e9894d0754
yaoyu bao <yaobao5354@gmail.com>
2019-09-21 00:21:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,14 +21,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     student_array = []
-    #doc.css("div.student-card")
     doc.css("div.student-card").each do |profile|
     student_hash = {
       :name => profile.css("h4.student-name").text,
       :location => profile.css("p.student-location").text,
       :profile_url => profile.css("a").first["href"]
     }
-    binding.pry 
     student_array << student_hash
     end
     student_array
